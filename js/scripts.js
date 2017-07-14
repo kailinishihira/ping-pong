6e2a114da11bab5fdd7302758db4e2b05f9d79c2
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-14 10:11:41 -0700

    Add non-functioning business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,36 @@ $(document).ready(function() {
   $("#ping-pong").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#numbers").val());
-
-debugger;
-
-    for (var i = 1; i <= userInput; i++) {
-      $(".output").append("<li>" + i + "</li>");
-    }
+    // for (var i = 1; i <= userInput; i++) {
+    //   $(".output").append("<li>" + i + "</li>");
+    // }
+    var userOutput = pingPongOutput(userInput);
+    $(".output").text("<li>" + userOutput + "</li>")
   });
  });
+
+var pingPongOutput = function(number) {
+  if (i % 3 === 0) {
+    i = "ping";
+  } else if (i % 5 === 0) {
+    i = "pong";
+  } else if (i % 15 === 0) {
+    i = "ping-pong"
+  }
+  
+  var returnValue = [];
+  for (var i = 1; i <= number; i++) {
+    returnValue.push(i);
+    }
+
+    for (var i = 0; i < returnValue.length; i++) {
+
+  }
+};
+
+
+
+
 
 
 
