591a22c75262f2be1c3737c00f91ed04ee5d9902
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-14 16:06:20 -0700

    Clean up the code alignments in the index and js.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,17 @@
 $(document).ready(function() {
   $("#ping-pong").submit(function(event) {
     event.preventDefault();
-
     var userInput = parseInt($("#numbers").val());
     var userOutput = pingPongOutput(userInput);
     $(".output").text(userOutput);
     });
   });
 
-
 var pingPongOutput = function(userInput) {
   var returnValue = [];
   for (var i = 1; i <= userInput; i++) {
     returnValue.push(i);
   }
-
   for (var i = 0; i < returnValue.length; i++) {
     if (returnValue[i] % 15 === 0) {
       returnValue.splice([i], 1, "pingpong")
