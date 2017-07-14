6e98fe7a8ad8a49ddf7cff4f82658f5e099a7ca1
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-14 09:49:12 -0700

    Add for loop to count up from 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
+
 $(document).ready(function() {
   $("#ping-pong").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#numbers").val());
-    $(".output").append(pingPongOutput(userInput));
+
+debugger;
+
+    for (var i = 1; i <= userInput; i++) {
+      $(".output").append("<li>" + i + "</li>");
+    }
   });
-});
+ });
 
-  var pingPongOutput = function(number) {
-    var returnValue = [];
-    for (var i = 0; i <= userInput; i-=1) {
 
-  }
+
+//     $(".output").append("<li>" + pingPongOutput(userInput) + "</li>");
+//
+//
+//   var pingPongOutput = function(number) {
+//     var returnValue = [];
+//     for (var i = 0; i < number; i-=1) {
+//       returnValue.push(i);
+//   }
+// } return returnValue;
