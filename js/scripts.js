4d50bccb36f098391dec68b4669e80e02b558533
Kaili Nishihira <kailinishihira@gmail.com>
2017-07-14 12:42:59 -0700

    Add functioning JS, and replace numbers divisible by 3 and 5 and replace with 'ping', 'pong', and 'ping-pong'.

--- js/scripts.js
+++ js/scripts.js
@@ -9,66 +9,20 @@ $(document).ready(function() {
     });
   });
 
-    // if (result === "ping") {
-    //   $(".output").text("<li>" + "ping" + "</li>");
-    // } else if (result === "pong") {
-    // $(".output").text("<li>" + "pong" + "</li>");
-    // } else if (result === "ping-pong")
-    // $(".output").text("ping-pong")
-    //
-    //
 
-    // for (var i = 1; i <= userInput; i++) {
-    //   // $(".output").append("<li>" + i + "</li>");
-
-//counts up from 1
 var pingPongOutput = function(userInput) {
-      debugger;
   var returnValue = [];
   for (var i = 1; i <= userInput; i++) {
     returnValue.push(i);
   }
 
   for (var i = 0; i < returnValue.length; i++) {
-    if (i % 3 === 0) {
-      returnValue.splice(returnValue[i], 1, "ping")
-    } else if (i % 5 === 0){
-      returnValue.splice(returnValue[i], 1, "pong")
-    } else if (i % 15 === 0) {
-      returnValue.splice(returnValue[i], 1, "ping-pong")
+    if (returnValue[i] % 15 === 0) {
+      returnValue.splice([i], 1, "pingpong")
+    } else if (returnValue[i] % 3 === 0){
+      returnValue.splice([i], 1, "ping")
+    } else if (returnValue[i] % 5 === 0) {
+      returnValue.splice([i], 1, "pong")
     }
-
+  } return returnValue.toString();
   }
-  }
-
-
-    // while (i < returnValue.length) {
-    //   if (i % 3 === 0) {
-    //   var returnPingPong = returnValue.splice([i], 1, "ping");
-    // }
-  // }
-
-//
-//
-
-//     } else if (i % 5 === 0) {
-//       var returnPingPong = returnValue.splice([i], 1, "pong");
-//       i+=2;
-//     } else if (i % 15 === 0) {
-//       var returnPingPong = returnValue.splice([i], 1, "ping-pong");
-//       i+=2;
-//     }
-//   }
-// }
-
-//implement ping pong
-// var pingPongOutput = function(number) {
-//     $(".output").append("<li>" + pingPongOutput(userInput) + "</li>");
-//
-//
-//   var pingPongOutput = function(number) {
-//     var returnValue = [];
-//     for (var i = 0; i < number; i-=1) {
-//       returnValue.push(i);
-//   }
-// } return returnValue;
